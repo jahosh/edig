ddaa409cd49b01d435a183ebd2ad94d9ef2f05ff
=josh91hickman <josh91hickman@gmail.com>
2017-08-05 14:59:43 -0700

    refresh

--- public/index.js
+++ public/index.js
@@ -1,58 +1,50 @@
-$("#download-button").hide();
+$(document).ready(function() {
+  $("#download-button").hide();
+  $('.tooltip').tooltipster();
+  
+  $("#e-dig-form").on("submit", (e) => {
+    e.preventDefault();
+    const link = $("#youtube-link-input").val();
+    const start = $("#video-begin-input").val();
+    const end = $("#video-end-input").val();
 
-
-$("#e-dig-form").on("submit", (e) => {
-  e.preventDefault();
-  console.log('submitted');
-
-  const link = $("#youtube-link-input").val();
-  const start = $("#video-begin-input").val();
-  const end = $("#video-end-input").val();
-
-
-  const c1 = start.replace(":", "")
-  const c2 = end.replace(":", "")
+    const c1 = start.replace(":", "")
+    const c2 = end.replace(":", "")
 
 
 
-  if (/^\d+$/.test(c1) || /^\d+$/.test(c2) ) {
-    console.log('yes');
-  } else {
-    alert('please use only stringifed numbers in your start/end times');
-    return;
+    if (/^\d+$/.test(c1) || /^\d+$/.test(c2) ) {
+      console.log('yes');
+    } else {
+      alert('please use only stringifed numbers in your start/end times');
+      return;
+    }
+
+    requestSample({ link, start, end});
+  });
+
+
+  function requestSample(payload) {
+    const { link, start, end } = payload;
+    $.ajax({
+      url: `/dig?src=${link}&start=${start}&end=${end}`,
+      error: (err) => {
+        console.log(err);
+      },
+      success: (resp) => {
+        $("#download-button").fadeIn("slow");
+        const link = $("#youtube-link-input").val('');
+        const start = $("#video-begin-input").val('0:00');
+        const end = $("#video-end-input").val('');
+        const result = JSON.parse(resp);
+        handleDownload(result.link);
+      } 
+    });
   }
 
-  console.log(c1, c2);
-  console.log(link, start, end);
-
-  requestSample({ link, start, end});
-});
-
-
-function requestSample(payload) {
-  const { link, start, end } = payload;
-  console.log(payload);
-  $.ajax({
-    url: `/dig?src=${link}&start=${start}&end=${end}`,
-    error: (err) => {
-      console.log(err);
-    },
-    success: (resp) => {
-      console.log('worked');
-      $("#download-button").fadeIn("slow");
-      const link = $("#youtube-link-input").val('');
-      const start = $("#video-begin-input").val('');
-      const end = $("#video-end-input").val('');
-      const result = JSON.parse(resp);
-      handleDownload(result.link);
-    } 
-  });
-}
-
-
-function handleDownload(link) {
-  console.log('this is link', link);
-  $("#download-button").click(() => {
-    window.open(`/download?link=${link}`);
-  });
-}
+  function handleDownload(link) {
+    $("#download-button").click(() => {
+      window.open(`/download?link=${link}`);
+    });
+  }
+})
