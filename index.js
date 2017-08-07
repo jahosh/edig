866d29e8d4703d7d84c478503ee65bae42281d2f
=josh91hickman <josh91hickman@gmail.com>
2017-08-06 20:48:59 -0700

    refresh v3

--- index.js
+++ index.js
@@ -10,6 +10,54 @@ const morgan = require('morgan');
 
 const app = express();
 
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+
+  app.get('/dig', (req, res) => {
+    const { src, start, end, full } = req.query;
+    
+    const downloadFull = Boolean(full);
+
+    const meta = getMetaData(src)
+      .then((metadata) => {
+        let title = metadata.title;
+        let length = metadata.length_seconds;
+        let thumbnail = metadata.thumbnail_url;
+        let endTime = end > length ? length : end;
+
+        dlVid(src, io)
+          .then((vid) => {
+            const randomId = randomGen();
+            const name = slugify(`${title}-${randomId}.mp3`);
+            const command = processVideo(vid, downloadFull, name, start, end, length);
+
+            command.run();
+            command.on('error', (err, stdout, stderr) => {
+              console.log(err.message, err, stderr);
+              res.send(JSON.stringify('error'))
+            });
+            command.on('end', () => {
+              res.end(JSON.stringify({
+                link: `temp/${name}`,
+                title,
+                thumbnail
+              }));
+            });
+          })
+          .catch(e => {
+            console.log(e);
+            res.end('error');
+          });
+      })
+      .catch(e => {
+        console.log('error here', e);
+        res.end(JSON.stringify({ e, msg: 'unable to locate video, please try another link' }));
+      });
+  });
+});
+
 app.set('view engine', 'ejs');
 app.use(compression())
 app.use(express.static(__dirname + '/public'));
@@ -19,50 +67,9 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/dig', (req, res) => {
-  const { src, start, end } = req.query;
-
-  const meta = getMetaData(src)
-    .then((metadata) => {
-      let title = metadata.title;
-      let length = metadata.length_seconds;
-      let thumbnail = metadata.thumbnail_url;
-      let endTime = end > length ? length : end;
-
-    dlVid(src)
-      .then((vid) => {
-        const randomId = randomGen();
-        const name = slugify(`${title}-${randomId}.mp3`);
-        const command = processVideo(vid, false, name, start, end);
-
-        command.run();
-        command.on('error', (err, stdout, stderr) => {
-          console.log(err.message, err, stderr);
-        });
-        command.on('end', () => {
-          res.end(JSON.stringify({
-            link: `temp/${name}`,
-            title,
-            thumbnail
-          }));
-        });
-      })
-      .catch(e => {
-        console.log(e);
-        res.send(JSON.stringify(e));
-      });
-    })
-    .catch(e => {
-      console.log(e);
-      res.send(JSON.stringify(e));
-    });
-});
-
 
 app.get('/download', (req, res) => {
   const { link } = req.query;
-  console.log('this is req.query', req.query)
-  console.log('this is link on server', link);
 
   res.download(link, function (err) {
     if (err) {
@@ -82,27 +89,35 @@ app.get('/download', (req, res) => {
       }
     }
   });
-})
-
-
-app.listen(3000, () => {
-  console.log('running on port 3000');
 });
 
+server.listen(3000, () => {
+  console.log('listening on 3000');
+});
 
-
-function dlVid(src) {
+function dlVid(src, socket) {
   return new Promise((fulfill, reject) => {
     try {
       const dl = ytdl(src, {
         filter: 'audioonly'
       });
-      dl.on('progress', (c, td, tdl) => {
-        console.log(c, td, tdl);
-      });
-      dl.on('info', (info) => {
-      })
 
+      dl.on('progress', (c, td, tdl) => {
+        const floatDownloaded = td/tdl;
+        let progress = (floatDownloaded * 100).toFixed(2);
+        socket.emit('progress', progress);
+      });
+      
+      dl.on('info', (info) => {
+        
+      });
+
+      dl.on('end', () => {
+        socket.emit('progress', 100);
+      });
+
+      dl.on('data', () => {
+      })
       fulfill(dl);
     } catch(e) {
       return reject(e);
@@ -126,11 +141,10 @@ function randomGen() {
   return Math.floor((Math.random() * 10000000) + 1);
 }
 
-
-
-function processVideo(vid, full, name, start, end) {
+function processVideo(vid, full, name, start, end, length) {
   if (full) {
     start = 0;
+    end = length;
   }
   console.log(start, end);
   return ffmpeg(vid)
--- public/index.js
+++ public/index.js
@@ -1,83 +1,166 @@
 $(document).ready(function() {
-  $("#download-button").hide();
+  var opts = {
+    lines: 13 // The number of lines to draw
+    , length: 28 // The length of each line
+    , width: 14 // The line thickness
+    , radius: 42 // The radius of the inner circle
+    , scale: .2 // Scales overall size of the spinner
+    , corners: 1 // Corner roundness (0..1)
+    , color: '#000' // #rgb or #rrggbb or array of colors
+    , opacity: 0.25 // Opacity of the lines
+    , rotate: 0 // The rotation offset
+    , direction: 1 // 1: clockwise, -1: counterclockwise
+    , speed: 0.7 // Rounds per second
+    , trail: 60 // Afterglow percentage
+    , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+    , zIndex: 2e9 // The z-index (defaults to 2000000000)
+    , className: 'spinner' // The CSS class to assign to the spinner
+    , top: '50%' // Top position relative to parent
+    , left: '50%' // Left position relative to parent
+    , shadow: false // Whether to render a shadow
+    , hwaccel: false // Whether to use hardware acceleration
+    , position: 'absolute' // Element positioning
+  }
+  
+  var socket = io.connect();
+  const nanobar = new Nanobar();
 
+  socket.on('progress', (progress) => {
+    nanobar.go(progress);
+  });
+  
+  $("#download-button").hide();
 
   $('.tooltip').tooltipster({
     side: 'left',
   });
 
+  $("#full-song-input").click(() => {
+    let toggle = true;
+    if ($("#full-song-input").is(":checked")) {
+      toggleTimeInputs(toggle);
+    } else {
+      toggle = false;
+      toggleTimeInputs(toggle);
+    }
+  });
 
   $("#e-dig-form").on("submit", (e) => {
     e.preventDefault();
+
     const link = $("#youtube-link-input").val();
     const start = $("#video-begin-input").val();
     const end = $("#video-end-input").val();
+    let full;
+
+    if (!link.includes("www.youtube.com")) {
+      alert('must be a url from youtube');
+      return;
+    }
+
+    if ($("#full-song-input").is(":checked")) {
+      full = true;
+    }
 
     if (link === '') {
       alert('must submit a link');
       return;
     }
 
-    const c1 = start.replace(":", "")
-    const c2 = end.replace(":", "")
+    if (!full && !start && !end) {
+      alert('must enter times');
+      return;
+    }
+
 
     const sTime = msToSecondsOnly(start);
     const eTime = msToSecondsOnly(end);
 
-
-    if (/^\d+$/.test(c1) || /^\d+$/.test(c2) ) {
-      console.log('yes');
+    if (/^\d+$/.test(sTime) || /^\d+$/.test(eTime) ) {
     } else {
       alert('please use only stringifed numbers in your start/end times');
       return;
     }
 
-    requestSample({ link, sTime, eTime});
+    if (sTime > eTime) {
+      alert('invalid times, start time must be before end time')
+      return;
+    }
+
+    const target = document.getElementById('loading')
+    const spinner = new Spinner(opts).spin(target);
+    $("#submit").hide();
+    requestSample({ link, sTime, eTime, full });
   });
 
 
   function requestSample(payload) {
-    const { link, sTime, eTime } = payload;
+    const { link, sTime, eTime, download } = payload;
+    let full = download ? true : false;
+
+    toggleInputs(true);
     $.ajax({
-      url: `/dig?src=${link}&start=${sTime}&end=${eTime}`,
+      url: `/dig?src=${link}&start=${sTime}&end=${eTime}&full=${full}`,
       error: (err) => {
         console.log(err);
       },
       success: (resp) => {
+        const result = JSON.parse(resp);
+
+        $("#loading").fadeOut("slow");
         $("#download-button").fadeIn("slow");
+        $("#next-button").fadeIn("slow");
+        $("#next-button").click(() => {
+          location.reload();
+        });
+
         const link = $("#youtube-link-input").val('');
         const start = $("#video-begin-input").val('0:00');
         const end = $("#video-end-input").val('');
-        console.log(resp);
-        const result = JSON.parse(resp);
-        showInfo(result.title);
+
+
+        showInfo(result.title, result.thumbnail);
         handleDownload(result.link);
       } 
     });
   }
 
   function handleDownload(link) {
-    console.log('this is link', `/download?link=${link}`);
     $("#download-button").click(() => {
       window.open(`/download?link=${link}`);
       $("#download-button").hide();
       location.reload();
+      $("#submit").toggle();
+      $("#download-button").toggle();
     });
   }
 
-  function showInfo(title) {
-    $("#track-title").append(title);
-  }
+  function showInfo(title, thumbnail) {
+    $("#track-title").text(title);
+    $("#track-image").html(`<img src='${thumbnail}' />`);
+  };
 
   function msToSecondsOnly(str) {
-    var p = str.split(':'),
-      s = 0, m = 1;
+    let p = str.split(':')
+    let s = 0
+    let m = 1;
 
     while (p.length > 0) {
       s += m * parseInt(p.pop(), 10);
       m *= 60;
     }
-
     return s;
   }
-})
+
+  function toggleInputs(flag) {
+    $("#youtube-link-input").prop('disabled', flag);
+    $("#video-begin-input").prop('disabled', flag);
+    $("#video-end-input").prop('disabled', flag);
+  }
+
+  function toggleTimeInputs(flag) {
+    $("#video-begin-input").prop('disabled', flag);
+    $("#video-end-input").prop('disabled', flag);
+  }
+
+});
